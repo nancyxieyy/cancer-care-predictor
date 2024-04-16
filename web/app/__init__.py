4fb6ade099e3dadaba320ff62790850756965ef2
“40381880” <yxie17@qub.ac.uk>
2024-04-16 19:56:06 +0100

    Update

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
+# from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
 
 def create_app():
     app = Flask(__name__)
-
-    app.secret_key = 'your_secret_key'  # 设置一个安全密钥
     load_dotenv()  # 确保这行代码在创建 Flask 应用实例之前
+
+    #app.secret_key = 'your_secret_key'  # 设置一个安全密钥
+    app.secret_key = os.getenv('SECRET_KEY')
+    
+    # 配置MySQL
+    # app.config['MYSQL_HOST'] = 'localhost'
+    # app.config['MYSQL_USER'] = 'root'
+    # app.config['MYSQL_PASSWORD'] = 'yy020529'
+    # app.config['MYSQL_DB'] = 'user_db'
+
+    # mysql = MySQL(app)
 
     from .views import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- web/app/views.py
+++ web/app/views.py
@@ -6,16 +6,20 @@ import os
 
 main = Blueprint('main', __name__)
 
+# Assumed username and password
+USERNAME = 'admin'
+PASSWORD = '123456'
+
 # Global variables are used to store the model
 survival_model = None
 disease_model = None
 treatment_model = None
 
 survival_scaler = None
-treatment_scaler = None
+disease_scaler = None
 
 def load_model():
-    global survival_model, disease_model, treatment_model, survival_scaler, treatment_scaler
+    global survival_model, disease_model, treatment_model, survival_scaler, disease_scaler
     
     # The paths of models
     survival_model_path = os.path.join(current_app.root_path, 'models', 'survival_model.pkl')
@@ -29,20 +33,22 @@ def load_model():
     
     # The paths of scalers
     survival_scaler_path = os.path.join(current_app.root_path, 'models', 'survival_scaler.pkl')
-    treatment_scaler_path = os.path.join(current_app.root_path, 'models', 'treatment_scaler.pkl')
+    disease_scaler_path = os.path.join(current_app.root_path, 'models', 'disease_scaler.pkl')
     
     # Load the scalers
     survival_scaler = joblib.load(survival_scaler_path)
-    treatment_scaler = joblib.load(treatment_scaler_path)
+    disease_scaler = joblib.load(disease_scaler_path)
 
 @main.before_app_first_request
 def before_first_request():
     load_model()
 
-# Assumed username and password
-USERNAME = 'admin'
-PASSWORD = '123456'
+@main.route('/')
+def index():
+    # Root path redirection to login page
+    return redirect(url_for('main.login'))  
 
+# Login
 @main.route('/login', methods=['GET', 'POST'])
 def login():
     if 'username' in session:
@@ -60,30 +66,28 @@ def login():
             return 'Invalid username or password'
     return render_template('login.html')
 
+# Logiout
 @main.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('main.login'))
 
+# Home
 @main.route('/home')
 def home():
     if 'username' in session:
         return render_template('home.html')
     return redirect(url_for('main.login'))
 
+# Chat
 @main.route('/chat')
 def chat():
     if 'username' in session:
         # Make sure users are logged in to access the chat page
         return render_template('chat.html')
     # Unlogged-in users redirected to login page
-    return redirect(url_for('main.login'))  
-
-@main.route('/')
-def index():
-    # Root path redirection to login page
-    return redirect(url_for('main.login'))  
-
+    return redirect(url_for('main.login')) 
+ 
 @main.route('/ask', methods=['POST'])
 def ask():
     if 'username' not in session:
@@ -121,11 +125,21 @@ def survival():
     prediction = None
 
     if request.method == 'POST':
+        chemo_type = str(request.form.get('chemo_type', 'N/A'))
+        
+        # Switching chemotherapy types according to mapping of chemotherapy types
+        chemo_type_mapping = {'N/A': 0, '5FU': 1, 'FOLFIRI': 2, 'FOLFOX': 3, 'FUFOL': 4, 'other': 5}
+        chemotherapy_adjuvant_type_numeric = chemo_type_mapping.get(chemo_type, 0)
+        
         form_data = {
             'age_at_diagnosis_in_years': int(request.form.get('age', 0)),
+            'sex_numeric': 0 if request.form.get('sex') == 'Male' else 1,
             'rfs_event': int(request.form.get('relapse', 0)),
             'rfs_months': int(request.form.get('relapse_months', 0)),
-            'os_months': int(request.form.get('death_months', 0))
+            'os_months': int(request.form.get('death_months', 0)),
+            'tnm_stage': int(request.form.get('stage', 0)), 
+            'chemotherapy_adjuvant_numeric': 0 if request.form.get('chemo') == 'No' else 1, 
+            'chemotherapy_adjuvant_type_numeric':int(chemotherapy_adjuvant_type_numeric)
         }
         
         global survival_model, survival_scaler
@@ -141,6 +155,57 @@ def survival():
 
 def predict_death_probability(model, input_features):
     prediction = model.predict_proba(input_features)[0][1]
+    return prediction
+
+# disease progression
+@main.route('/disease', methods=['GET', 'POST'])
+def disease():
+    if 'username' not in session:
+        return redirect(url_for('main.login'))
+
+    if request.method == 'POST':
+        try:
+            chemo_type = request.form.get('chemo_type', 'N/A')
+            cms = request.form.get('cms', 'UNK')
+            
+            chemo_type_mapping = {'N/A': 0, '5FU': 1, 'FOLFIRI': 2, 'FOLFOX': 3, 'FUFOL': 4, 'other': 5}
+            chemotherapy_adjuvant_type_numeric = chemo_type_mapping.get(chemo_type, 0)
+            cms_mapping = {'CMS1': 1, 'CMS2': 2, 'CMS3': 3, 'CMS4': 4, 'UNK': 0}
+            CMS_numeric = cms_mapping.get(cms, 0)
+            
+            form_data = {
+                'rfs_months': int(request.form.get('relapse_months', 0)),
+                'os_months': int(request.form.get('death_months', 0)),
+                'age_at_diagnosis_in_years': int(request.form.get('age', 0)),
+                'sex_numeric': 0 if request.form.get('sex') == 'Male' else 1,
+                'tnm_stage': int(request.form.get('stage', 0)), 
+                'chemotherapy_adjuvant_numeric': 0 if request.form.get('chemo') == 'No' else 1, 
+                'chemotherapy_adjuvant_type_numeric': chemotherapy_adjuvant_type_numeric,
+                'CMS_numeric': CMS_numeric
+            }
+            
+            global disease_model, disease_scaler
+            input_df = disease_scaler.transform(pd.DataFrame([form_data]))
+            if disease_model is not None:
+                prediction = predict_progression_probability(disease_model, input_df)
+                predictions = {
+                    0: 'Stabilisation in condition',
+                    1: 'Improvement in condition',
+                    2: 'Worsening of the condition',
+                    3: 'Pathological changes'
+                }
+                prediction_text = predictions.get(prediction, 'Unknown')
+                return jsonify({'prediction': prediction_text})
+            else:
+                return jsonify({'error': 'Model could not be loaded'}), 500
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+    return render_template('disease.html')
+
+def predict_progression_probability(model, input_features):
+    # Suppose the model returns a category label
+    prediction = model.predict(input_features)[0]  
     return prediction
 
 # relapse rate(treatment response)
@@ -164,6 +229,8 @@ def treatment():
         CMS_numeric = cms_mapping.get(cms, 0)
         
         form_data = {
+            'os_event': 0,
+            'os_months': int(request.form.get('death_months', 0)),
             'age_at_diagnosis_in_years': int(request.form.get('age', 0)),
             'sex_numeric': 0 if request.form.get('sex') == 'Male' else 1,
             'tnm_stage': int(request.form.get('stage', 0)), 
@@ -172,8 +239,8 @@ def treatment():
             'CMS_numeric': int(CMS_numeric)
         }
         
-        global treatment_model, treatment_scaler
-        input_df = treatment_scaler.transform(pd.DataFrame([form_data]))
+        global treatment_model
+        input_df = pd.DataFrame([form_data])
         if treatment_model is not None:
             prediction = predict_relapse_probability(treatment_model, input_df)
             prediction = round(prediction * 100, 2)
@@ -185,55 +252,4 @@ def treatment():
 
 def predict_relapse_probability(model, input_features):
     prediction = model.predict_proba(input_features)[0][1]
-    return prediction
-
-# disease progression
-@main.route('/disease', methods=['GET', 'POST'])
-def disease():
-    if 'username' not in session:
-        return redirect(url_for('main.login'))
-
-    if request.method == 'POST':
-        try:
-            chemo_type = request.form.get('chemo_type', 'N/A')
-            cms = request.form.get('cms', 'UNK')
-            
-            chemo_type_mapping = {'N/A': 0, '5FU': 1, 'FOLFIRI': 2, 'FOLFOX': 3, 'FUFOL': 4, 'other': 5}
-            chemotherapy_adjuvant_type_numeric = chemo_type_mapping.get(chemo_type, 0)
-            cms_mapping = {'CMS1': 1, 'CMS2': 2, 'CMS3': 3, 'CMS4': 4, 'UNK': 0}
-            CMS_numeric = cms_mapping.get(cms, 0)
-            
-            form_data = {
-                'age_at_diagnosis_in_years': int(request.form.get('age', 0)),
-                'sex_numeric': 0 if request.form.get('sex') == 'Male' else 1,
-                'tnm_stage': int(request.form.get('stage', 0)), 
-                'chemotherapy_adjuvant_numeric': 0 if request.form.get('chemo') == 'No' else 1, 
-                'chemotherapy_adjuvant_type_numeric': chemotherapy_adjuvant_type_numeric,
-                'CMS_numeric': CMS_numeric
-            }
-            
-            print(form_data)
-            
-            global disease_model
-            input_df = pd.DataFrame([form_data])
-            if disease_model is not None:
-                prediction = predict_progression_probability(disease_model, input_df)
-                predictions = {
-                    0: 'Stabilisation in condition',
-                    1: 'Improvement in condition',
-                    2: 'Worsening of the condition',
-                    3: 'Pathological changes'
-                }
-                prediction_text = predictions.get(prediction, 'Unknown')
-                return jsonify({'prediction': prediction_text})
-            else:
-                return jsonify({'error': 'Model could not be loaded'}), 500
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
-
-    return render_template('disease.html')
-
-def predict_progression_probability(model, input_features):
-    # Suppose the model returns a category label
-    prediction = model.predict(input_features)[0]  
     return prediction
